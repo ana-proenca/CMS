a19bc3b34dcff6d40bd701c963b284ab4490d1a0
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-03-25 07:02:16 -0300

    Create StudentReport.java

---
+++ src/cms/StudentReport.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms;
+
+/**
+ *
+ * @author anaclaudiaproenca
+ */
+public class StudentReport {
+    private String studentId;
+    private String studentName;
+    private String studentSurname;
+    private String courseName;
+    private String moduleName;
+    private int grade;
+    private String moduleCompleted;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentSurname() {
+        return studentSurname;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getModuleCompleted() {
+        return moduleCompleted;
+    }
+}
