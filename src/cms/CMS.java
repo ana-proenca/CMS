2a12cb0af3b1e4850283970086c98934dfed3dcc
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-03-25 19:33:50 -0300

    add login user

--- src/cms/CMS.java
+++ src/cms/CMS.java
@@ -4,6 +4,8 @@
  */
 package cms;
 
+import java.util.Scanner;
+
 /**
  *
  * @author anaclaudiaproenca
@@ -14,7 +16,33 @@ public class CMS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        User userLogin = login(sc);
+        
         // TODO code application logic here
+        DBConnector db = new DBConnector();
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
+    private static User login(Scanner sc) {
+        System.out.println("Inform an username");
+        String username = sc.next();
+
+        System.out.println("Inform a password");
+        String password = sc.next();
+        try {
+            DBConnector db = new DBConnector();
+            
+            User user = db.login(username, password);
+            
+            return user;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
