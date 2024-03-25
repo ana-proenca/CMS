6b0b4364a29cf98a22d7e6b0b541899c2c272651
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-03-24 21:40:24 -0300

    Initial commit

---
+++ src/DBConnector.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author anaclaudiaproenca
+ */
+public class DBConnector {
+    private final String DB_URL = "jdbc:mysql://127.0.0.1";
+    private final String USER = "pooa2024";
+    private final String PASSWORD = "pooa2024";
+}
