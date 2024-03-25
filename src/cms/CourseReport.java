0c86fbb50f410405274b0836293b3af58e3f005a
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-03-24 21:46:35 -0300

    Create CourseReport.java

---
+++ src/cms/CourseReport.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms;
+
+/**
+ *
+ * @author anaclaudiaproenca
+ */
+public class CourseReport {
+    private String courseName;
+    private String moduleName;
+    private String numberOfStudents;
+    private String lecturerName;
+    private String roomName;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(String numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public String getLecturerName() {
+        return lecturerName;
+    }
+
+    public void setLecturerName(String lecturerName) {
+        this.lecturerName = lecturerName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+    
+}
