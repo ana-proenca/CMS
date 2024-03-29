d3d17bf57213c80e73364ae8aed8aa3488b2eb6c
Ana Claudia de Proenca <ac-proenca@hotmai.com>
2024-03-29 15:51:52 -0300

    adding comment

--- src/cms/User.java
+++ src/cms/User.java
@@ -5,7 +5,7 @@
 package cms;
 
 /**
- *
+ * Class user represents the user table from MySql
  * @author anaclaudiaproenca
  */
 public class User {
@@ -15,36 +15,72 @@ public class User {
     private String role;
     private int user_id;
 
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
+    /**
+     * Constructor that initializes the User class with username, password and role defined
+     * @param username
+     * @param password
+     * @param role 
+     */
     public User(String username, String password, String role) {
         this.username = username;
         this.password = password;
         this.role = role;
     }
 
+    /**
+     * Method to return the userId
+     *
+     * @return
+     */
+    public int getUser_id() {
+        return user_id;
+    }
+
+    /**
+     * Method to set the userId during the query in the data based
+     *
+     * @param user_id
+     */
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    /**
+     * method to get user name
+     * @return 
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * method to get password
+     * @return 
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * method to get role
+     * @return 
+     */
     public String getRole() {
         return role;
     }
 
+    /**
+     * method to set username
+     * @param username 
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * method to set password
+     * @param password 
+     */
     public void setPassword(String password) {
         this.password = password;
     }
